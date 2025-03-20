4d6e649e16b6611cead23e3980cf3acf85176a1a
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-20 18:24:48 -0300

    Initial folder structure

---
+++ src/test/java/dev/marcostavares/clients_api/ClientsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcostavares.clients_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
