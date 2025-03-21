1c430670e437b612557e1330b0a58b92d30d6f4b
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-21 10:41:42 -0300

    feat: Create the CreateClient use case for initial testing

--- src/main/java/dev/marcostavares/clients_api/application/useCases/CreateClient.java
+++ src/main/java/dev/marcostavares/clients_api/application/useCases/CreateClient.java
@@ -1,5 +1,25 @@
 package dev.marcostavares.clients_api.application.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.marcostavares.clients_api.domain.model.Client;
+import dev.marcostavares.clients_api.domain.repository.ClientRepository;
+
+@Service
 public class CreateClient {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client execute(Client clientToBeCreated) {
+        this.clientRepository
+                .findByCPF(clientToBeCreated.getCPF())
+                .ifPresent(client -> {
+                    throw new IllegalArgumentException("CPF already registered");
+                });
+
+        return this.clientRepository.save(clientToBeCreated);
+    }
 
 }
